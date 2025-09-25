f67afb537a38da0830fafefba3bc40295bbc1354
Arnaudguetch <arnaud.guetch@gmail.com>
2025-09-26 00:03:38 +0200

    modified all files-x32 again

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -5,17 +5,19 @@ set -e
 set -x
 
 echo "Lancement du script ML (main.py)..."
-python3 /source/main.py
+python3 /source/main.py &
 
 echo "Fin du lancement de main.py..."
 
 echo "Lancement du script de sauvegarde des data...."
-python3 /data/save_files.py
+python3 /data/save_files.py &
 
 echo "Lancement du script de labase de données..."
-python3 /data/database.py
+python3 /data/database.py &
 
 echo "Fin du lancement de tous les scripts..."
+
+wait
 
 
 
