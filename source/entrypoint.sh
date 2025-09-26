bfe8b3a224215f24fe13881e854580d5f20657fd
Arnaudguetch <arnaud.guetch@gmail.com>
2025-09-26 12:00:11 +0200

    modified all files-x54 again

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
-
 set -e
-
 set -x
 
 mkdir -p /logs
 
-echo "Lancement des scripts en parallèle..."
-
 echo "Lancement du script ML (main.py)..."
-python3 /source/main.py > /logs/main.log 2>&1 &
+python3 /source/main.py > /logs/main.log 2>&1
 
-echo "Lancement du script de sauvegarde des data...."
-python3 /data/save_files.py > /logs/save_files.log 2>&1 &
+echo "Lancement du script de sauvegarde des data..."
+python3 /data/save_files.py > /logs/save_files.log 2>&1
 
-echo "Lancement du script de labase de données..."
-python3 /data/database.py > /logs/database.log 2>&1 &
+echo "Lancement du script de la base de données..."
+python3 /data/database.py > /logs/database.log 2>&1
 
-echo "Fin du lancement de tous les scripts..."
-
-wait
-
-
-
-
-
+echo "Tous les scripts ont été exécutés."
